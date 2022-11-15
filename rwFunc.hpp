9c28d4a89f1af14eb4066502ee89ae8f8d6e5cce
Алексей Ильин <thealexsnare@yandex.ru>
2022-11-15 10:47:46 +0300

    Создана структура данных

--- rwFunc.hpp
+++ rwFunc.hpp
@@ -1,4 +1,16 @@
 #ifndef RWFUNC_HPP
 #define RWFUNC_HPP
+#include <iostream>
+
+typedef struct Tfilms
+{
+    int id;
+    std::string title;
+    std::string studio;
+    std::string  genre;
+    int year_publishing;
+    bool inStock;
+    int year_purchasing;
+} Films;
 
 #endif // RWFUNC_HPP
